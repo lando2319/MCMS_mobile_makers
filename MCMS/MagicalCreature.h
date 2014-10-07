bdc64286a5cbe551b0ce55dd8eb1c3844889b4f0
Mike Land <mikepland@gmail.com>
2014-10-07 13:58:01 -0500

    finishes step one

---
+++ MCMS/MagicalCreature.h
@@ -0,0 +1,15 @@
+//
+//  MagicalCreature.h
+//  MCMS
+//
+//  Created by MIKE LAND on 10/7/14.
+//  Copyright (c) 2014 MIKE LAND. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MagicalCreature : NSObject
+
+@property NSString *name;
+
+@end
--- MCMS/ViewController.m
+++ MCMS/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "MagicalCreature.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    MagicalCreature *bigMonster = [[MagicalCreature alloc] init];
+    MagicalCreature *smallMonster = [[MagicalCreature alloc] init];
+    MagicalCreature *mediumMonster = [[MagicalCreature alloc] init];
+    self.creatures = [NSMutableArray arrayWithObjects:bigMonster, smallMonster, mediumMonster, nil];
 }
 
 - (void)didReceiveMemoryWarning {
