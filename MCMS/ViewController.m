a2894503639e3cb835bd8f7b180c04fb651bbec9
Mike Land <mikepland@gmail.com>
2014-10-07 15:04:08 -0500

    completes step 4

--- MCMS/ViewController.m
+++ MCMS/ViewController.m
@@ -9,7 +9,9 @@
 #import "ViewController.h"
 #import "MagicalCreature.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *textForCreature;
+@property (weak, nonatomic) IBOutlet UITableView *monsterTableView;
 
 @end
 
@@ -19,10 +21,46 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     MagicalCreature *bigMonster = [[MagicalCreature alloc] init];
+    bigMonster.name = @"Bill";
     MagicalCreature *smallMonster = [[MagicalCreature alloc] init];
+    smallMonster.name = @"Wack";
     MagicalCreature *mediumMonster = [[MagicalCreature alloc] init];
+    mediumMonster.name = @"Tim";
     self.creatures = [NSMutableArray arrayWithObjects:bigMonster, smallMonster, mediumMonster, nil];
+    NSLog(@"%@", smallMonster.name);
+
 }
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.creatures.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MyCellID" forIndexPath:indexPath];
+
+
+
+    NSLog(@"%@", cell);
+    MagicalCreature *creatureActual = [self.creatures objectAtIndex:indexPath.row];
+    cell.textLabel.text = creatureActual.name;
+    return cell;
+}
+
+- (IBAction)addCreature:(id)sender {
+
+    MagicalCreature *newMonster = [[MagicalCreature alloc] init];
+    newMonster.name = self.textForCreature.text;
+    [self.creatures addObject:newMonster];
+    [self.monsterTableView reloadData];
+    self.textForCreature.text = @"";
+    
+
+
+
+}
+
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
